553b4dfe09e24f3ac925e17fa7f0f935be3f0dcc
Khomitsevich Nikita <hamsternik9@gmail.com>
2016-04-29 13:21:59 +0300

    Fixed deploy script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Start to deploy all dotfiles into checked system"
 #DOTFILES=".Xresources .aliases .bash_profile .bashrc .gitattributes .gitconfig .gitignore .vimrc .zshenv .zshrc"
 FILES=`ls -A | grep "^\."`
 
-for FILE in DOTFILES; do
-	cp -r FILE "${HOME}/"
+for FILE in $FILES; do
+	cp -r $FILE "${HOME}/folder/"
 done
 
