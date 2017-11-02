e5222173963a0535f024464db05807857009429e
Nikita Khomitsevich <hamsternik9@gmail.com>
2017-11-02 17:50:01 +0200

    [macos] Update tools from brew

--- macos/brew.sh
+++ macos/brew.sh
@@ -6,28 +6,31 @@ brew update
 # Make upgrade of all already-installed formulae
 brew upgrade --all
 
-# Emacs installation
-brew install --with-cocoa --srgb emacs
-# brew linkapp emacs    ### Don't use it! It disturbs to install Emacs.app into Application
-
 # Installation
 brew install appledoc \
-     carthage \
-     diff-so-fancy \
-     gradle \
-     htop \
-     tree \
-     maven \
-     mono \
-     openssl \
-     python3 \
-     watchman
+    carthage \
+    diff-so-fancy \
+    gradle \
+    htop \
+    tree \
+    maven \
+    mono \
+    openssl \
+    python3 \
+    watchman
 
 # Adding Taps
 brew tap caskroom/cask
 
-# Cask install
-#brew cask install emacs
+### Ruby
+brew install ruby 
+brew unlink ruby && brew link ruby # Link the latest ruby version
+# brew link --overwrite ruby # If ruby won't be linked and setuped as default in the system, run this
+
+### Emacs
+# brew install --with-cocoa --srgb emacs
+# brew linkapp emacs    ### Don't use it! It disturbs to install Emacs.app into Application
+# brew cask install emacs # Cask install
 
 # Remove outdate data
 brew cleanup
