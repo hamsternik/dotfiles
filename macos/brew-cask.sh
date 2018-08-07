c319762f7c4cb80f94075253104f6cde81d6647d
Nikita Khomitsevich <hamsternik9@gmail.com>
2018-08-07 13:55:00 +0300

    [macos:brew] Validate cask tapped core repo. Add new dependecies

--- macos/brew-cask.sh
+++ macos/brew-cask.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-brew tap caskroom/cask
-brew install brew-cask
+if ! [[ $(brew tap | grep -i phinze/cask) ]] ; then
+    echo "Will tap _phinze/homebrew-cask_ repo"
+    brew tap phinze/homebrew-cask
+fi
 
-brew update 
 brew upgrade brew-cask
 
 brew cask install \
     docker \
+    dropbox \
     google-chrome \
     iterm2 \
     java \
     qlmarkdown \
     soundcleod \
-    vlc
+    vlc \
+    zeplin
 
 brew cask cleaup
 
