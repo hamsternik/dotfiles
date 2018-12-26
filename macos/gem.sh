e8a0435fa07a828612f61d733b8348002a1fae15
Nikita Khomitsevich <hamsternik9@gmail.com>
2018-12-26 15:22:34 +0200

    [macos:gem] added fastlane plugins wip

--- macos/gem.sh
+++ macos/gem.sh
@@ -3,5 +3,10 @@
 gem install \
     xcpretty \
     cocoapods \
-    bundler
+    bundler \
+
+### TODO: `gem install fastlane-plugin-update_provisioning_profile_specifier`
+### TODO: `gem install fastlane-plugin-versioning`
+### TODO: `gem install fastlane-plugin-badge`
+### TODO: `gem install fastlane-plugin-slack_train`
 
