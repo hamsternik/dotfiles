4bb6e8cd192634c3ee92862b6b88aadec7e947f5
Nikita Khomitsevich <hamsternik9@gmail.com>
2019-09-20 07:42:09 +0300

    [brew] updated brew deploy script

--- macos/brew.sh
+++ macos/brew.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ "$(uname -s)" != "Darwin" ]; then
+    echo "Run brew only at macOS system!"
+    exit -1
+fi
+
 if [[ ! -f '/usr/local/bin/brew' ]] ; then
-    echo "Will install Homebrew for local user...\n"
+    echo "===> Installing Homebrew for @$USER user..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew bundle
+brew bundle check >/dev/null 2>&1 || {
+    echo "===> Installing Homebrew dependecies..."
+    brew bundle
+}
+
 brew cleanup
