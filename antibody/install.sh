ee80b22085ed570a2888f3724825524eb9dd3b57
Nikita Khomitsevich <hamsternik9@gmail.com>
2020-10-24 15:58:21 +0300

    [antibody] rename file with plugins; update install path at the script

--- antibody/install.sh
+++ antibody/install.sh
@@ -7,5 +7,5 @@ if command -v brew >/dev/null 2>&1; then
 # 	curl -sL https://git.io/antibody | sh -s
 fi
 
-antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+antibody bundle <"$DOTFILES/antibody/plugins.txt" >~/.zsh_plugins.sh
 antibody update
