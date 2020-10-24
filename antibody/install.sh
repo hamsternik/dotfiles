10f33da780f7dabad439e104455a9d680a72a873
Nikita Khomitsevich <hamsternik9@gmail.com>
2020-10-24 18:29:40 +0300

    [antibody] remove commented code

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,8 +3,6 @@
 if command -v brew >/dev/null 2>&1; then
 	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
 	brew install antibody
-# else
-# 	curl -sL https://git.io/antibody | sh -s
 fi
 
 antibody bundle <"$DOTFILES/antibody/plugins.txt" >~/.zsh_plugins.sh
