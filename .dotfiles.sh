eb9ba22554cc8b5f15db57c7b7e5c478fd15b3d1
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-04-30 04:58:37 +0300

    [deploy] refactor .dotfiles.sh; remove .zsh and .vim dirs

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -1,101 +1,59 @@
 #!/bin/bash
 
-# ---------------------- INPUT SCOPE ---------------------- #
-HOMEPATH="${HOME}"
-FILES="
-.aliases
-.editorconfig
-.gitattributes
-.gitconfig
-.gitignore_global
-.lfrc
-.tmux.conf
-.vim
-.vimrc
-.zsh
-.zshenv
-.zshrc
-"
-# ---------------------- END SCOPE ---------------------- #
+config_files=(
+"aliases"
+"editorconfig"
+"gitattributes"
+"gitconfig"
+"gitignore_global"
+"lfrc"
+"tmux.conf"
+"vimrc"
+"zshenv"
+"zshrc"
+)
 
-# ---------------------- FUNCTION SCOPE ---------------------- #
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-RESET='\033[0m'
-
-function log_start {
-    local RESULT=""
-    if [[ -n "$1" ]]; then
-        RESULT="${CYAN}Start${RESET} ${GREEN}${1}${RESET} ${CYAN}Scope${RESET}"
-    else
-        RESULT="${CYAN}Start Scope${RESET}"
-    fi
-    echo -e "----------------------- ${RESULT} ------------------------"
-}
-
-function log_finish {
-    local RESULT=""
-    if [[ -n "$1" ]]; then
-        RESULT="${CYAN}Finish${RESET} ${GREEN}${1}${RESET} ${CYAN}Scope${RESET}"
-    else
-        RESULT="${CYAN}Finish Scope${RESET}"
-    fi
-    echo -e "------------------------ ${RESULT} ------------------------"; echo
+dotfiles_echo() {
+  local fmt="$1"; shift
+  # shellcheck disable=SC2059
+  printf "[dotfiles] ${fmt}\\n" "$@"
 }
 
 function install_dotfiles {
-    log_start "${FUNCNAME[0]}"
-    ITEMS=$1
-    for ITEM in $ITEMS; do
-        SOURCE=$(pwd)/configs/$ITEM
-        TARGET=$HOMEPATH/$ITEM
-
-        if [[ -e $TARGET ]]; then
-            printf "%s is exist. Skipping...\n" "$ITEM"
-        elif [[ "$ITEM" == ".lfrc" ]]; then
-            if [[ ! -d "$HOMEPATH/.config/lf" ]]; then
-                mkdir -p "$HOMEPATH"/.config/lf
+    dotfiles_echo "-> 🚀🚀🚀 Linking basic dotfiles."
+    sleep 1 ### personal UI decision.
+    for file in "${config_files[@]}"; do
+        SOURCE=$(pwd)/configs/${file}
+        dotfiles_echo "✅ Linking .${file}"
+        if [[ "${file}" == "lfrc" ]]; then
+            if [[ ! -d "${HOME}/.config/lf" ]]; then
+                mkdir -p "${HOME}"/.config/lf
             fi
-            ln -v -s "$SOURCE" "$HOMEPATH/.config/lf/lfrc"
+            ln -nfs "${SOURCE}" "${HOME}/.config/lf/.${file}"
         else
-            ln -v -s "$SOURCE" "$TARGET"
-            printf "%s symlink was created successfully!\n" "$ITEM"
+            ln -nfs "${SOURCE}" "${HOME}/.${file}"
         fi
     done
-    log_finish "${FUNCNAME[0]}"
 }
 
 function uninstall_dotfiles {
-    log_start "${FUNCNAME[0]}"
-    ITEMS="$FILES"
-    for ITEM in $ITEMS; do
-        TARGET=$HOMEPATH/$ITEM
-
-        if [[ -L $TARGET ]]; then
-            unlink "$TARGET"
-            printf "%s -- symlink REMOVED from %s\n" "$ITEM" "$HOMEPATH"
-        elif [[ "$ITEM" == ".lfrc" ]]; then
-            if [[ -d "$HOMEPATH/.config/lf" ]]; then
-                unlink "$HOMEPATH/.config/lf/lfrc"
-                rm -rf "$HOMEPATH/.config"
-                printf "%s -- .lfrc symlink & .config folder REMOVED from %s\n" "$ITEM" "$HOMEPATH"
-            else
-            	printf "%s -- .lfrc symlink does not exist at %s/.config/lf directory\n" "$ITEM" "$HOMEPATH"
-            fi
-        else
-            printf "%s -- symlink does not exist at %s\n" "$ITEM" "$HOMEPATH"
+    dotfiles_echo "-> 🚀🚀🚀 Linking basic dotfiles."
+    sleep 1 ### personal UI decision.
+    for file in "${config_files[@]}"; do
+        if [[ -L "${HOME}/.${file}" ]]; then    
+            dotfiles_echo "☑️  Symbolic link detected. Removing..."
+            rm -rf "${HOME}/.${file}"
+        elif [[ -L "${HOME}/.config/lf/.${file}" ]]; then
+            dotfiles_echo "☑️  Symbolic link detected. Removing..."
+            rm -rf "$HOMEPATH/.config/lf"
         fi
     done
-    log_finish "${FUNCNAME[0]}"
 }
-# ---------------------- END SCOPE ---------------------- #
 
-
-# ---------------------- MAIN: EXECUTION ---------------------- #
 while [ $# -gt 0 ]; do
     case $1 in
         -i | --install)
-            install_dotfiles "$FILES" ;;
+            install_dotfiles ;;
         -u | --uninstall)
             uninstall_dotfiles ;;
     esac
@@ -104,3 +62,4 @@ done
 
 # References
 # https://github.com/webpro/dotfiles/blob/master/bin/dotfiles
+# https://github.com/joshukraine/dotfiles/blob/master/install.sh
