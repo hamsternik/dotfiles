2616724f7c9e42e49c252e747ea77875ca5ddbd2
Nikita Khomitsevych <hamsternik9@gmail.com>
2022-06-03 01:53:50 +0300

    env: fish init config

---
+++ configs/fish/config.fish
@@ -0,0 +1,33 @@
+# Navigation
+function ..     ; cd .. ; end
+function ...    ; cd ../.. ; end
+function ....   ; cd ../../.. ; end
+function l      ; ls -CF ; end
+function la     ; ls -A ; end
+function ll     ; ls -Al ; end
+
+# Utilities
+function g      ; git $argv ; end
+function cl     ; command clear ; end
+function grep   ; command grep --color=auto $argv ; end
+function tmux   ; command tmux -2 $argv ; end
+function ip     ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
+function piing  ; command ping -c 4 google.com ; end
+
+# Developer Paths Navigation
+set -x DEVELOPER ~/Developer
+set -x PUBLIC $DEVELOPER/public
+
+function dev;       cd $DEVELOPER;          end
+function public;    cd $PUBLIC;             end
+function private;   cd $DEVELOPER/private;  end
+function remote;    cd $DEVELOPER/remote;   end
+
+function b;         cd $PUBLIC/blog;        end
+function d;         cd $PUBLIC/dotfiles;    end
+function work;      cd remote/fluxon;       end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    status --is-interactive; and rbenv init - fish | source
+end
