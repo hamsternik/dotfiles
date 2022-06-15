7eb1c375d6f25df6c48e6cb2464332914991b3f6
Nikita Khomitsevych <hamsternik9@gmail.com>
2022-06-15 15:17:32 +0300

    fish: update conf

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,17 @@
-# Navigation
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    status --is-interactive; and rbenv init - fish | source
+end
+
+## PATH Overriding. Basics
+set -gx DEVELOPER ~/Developer
+set -gx PUBLIC $DEVELOPER/public
+set -gx REMOTE $DEVELOPER/remote
+
+## PATH Overriding. Programming Environment
+set -x NVM_DIR $HOME/.nvm ## put NVM to the fish PATH
+
+## Aliases. Navigation
 function ..     ; cd .. ; end
 function ...    ; cd ../.. ; end
 function ....   ; cd ../../.. ; end
@@ -6,19 +19,15 @@ function l      ; ls -CF ; end
 function la     ; ls -A ; end
 function ll     ; ls -Al ; end
 
-# Utilities
+## Aliases. Utilities
 function g      ; git $argv ; end
 function cl     ; command clear ; end
 function grep   ; command grep --color=auto $argv ; end
 function tmux   ; command tmux -2 $argv ; end
 function ip     ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
-function piing  ; command ping -c 4 google.com ; end
+function ping1  ; command ping -c 4 google.com ; end
 
-# Developer Paths Navigation
-set -gx DEVELOPER ~/Developer
-set -gx PUBLIC $DEVELOPER/public
-set -gx REMOTE $DEVELOPER/remote
-
+## Aliases. Programming Environment
 function dev;       cd $DEVELOPER;          end
 function public;    cd $PUBLIC;             end
 function private;   cd $DEVELOPER/private;  end
@@ -28,10 +37,3 @@ function b;         cd $PUBLIC/blog;        end
 function d;         cd $PUBLIC/dotfiles;    end
 function work;      cd $REMOTE/fluxon;      end
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    status --is-interactive; and rbenv init - fish | source
-end
-
-# Programming Env
-set -x NVM_DIR $HOME/.nvm ## put NVM to the fish PATH
