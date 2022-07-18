539a2037f452abfe6b7580aa3665e0627bf70925
Nikita Khomitsevych <hamsternik9@gmail.com>
2022-07-18 14:21:13 +0300

    fish: update config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -3,61 +3,68 @@ if status is-interactive
     status --is-interactive; and rbenv init - fish | source
 end
 
-## PATH Overriding. Basics
+set TERM "xterm-256color"               # Sets the terminal type
+set EDITOR "emacsclient -t -a ''"       # $EDITOR use Emacs in terminal
+set VISUAL "emacsclient -c -a emacs"    # $VISUAL use Emacs in GUI mode
+
 set -gx DEVELOPER ~/Developer
 set -gx PUBLIC $DEVELOPER/public
 set -gx REMOTE $DEVELOPER/remote
 
-## PATH Overriding. Programming Environment
+### PATH | Overriding
 
-## Doom Emacs
+# Doom Emacs
 fish_add_path ~/.emacs.d/bin
 alias e="emacsclient -c -a 'emacs'"
 
-## Node.js
+# Node.js
 fish_add_path /opt/homebrew/opt/node@16/bin
 set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include"
 
-## NVM
+# NVM
 set -x NVM_DIR $HOME/.nvm ## put NVM to the fish PATH
 fish_add_path $NVM_DIR
 
 # right now settle from shell by command `set --universal nvm_default_version lts`
 # set -gx nvm_default_version lts
 
-# prerequisite: brew install --cask android-studio
-# HOW TO SET JAVA_HOME IN MAC OS X HIGH SIERRA WITH FISH SHELL
-# https://www.alex-arriaga.com/how-to-set-java_home-in-mac-os-x-high-sierra-with-fish-shell/
+# Java
+## prerequisite: brew install --cask android-studio
+## HOW TO SET JAVA_HOME IN MAC OS X HIGH SIERRA WITH FISH SHELL
+## https://www.alex-arriaga.com/how-to-set-java_home-in-mac-os-x-high-sierra-with-fish-shell/
 export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/Contents/Home
 fish_add_path $JAVA_HOME
 
+# Android
 export ANDROID_HOME=$HOME/Library/Android/sdk
 fish_add_path $ANDROID_HOME/platform-tools ## add android `adb` and `fastboot` to the PATH
 
-## Aliases. Navigation
-function ..     ; cd .. ; end
-function ...    ; cd ../.. ; end
-function ....   ; cd ../../.. ; end
-function l      ; ls -CF ; end
-function la     ; ls -A ; end
-function ll     ; ls -Al ; end
+### Aliases
 
-## Aliases. Utilities
-function g      ; git $argv ; end
-function cl     ; command clear ; end
-function grep   ; command grep --color=auto $argv ; end
-function tmux   ; command tmux -2 $argv ; end
+# Navigation
+function ..     ; cd .. ;               end
+function ...    ; cd ../.. ;            end
+function ....   ; cd ../../.. ;         end
+function l      ; ls -CF ;              end
+function la     ; ls -A ;               end
+function ll     ; ls -Al ;              end
+
+# Utilities
+function g      ; git $argv ;           end
+function cl     ; command clear ;       end
+function grep   ; command grep --color=auto $argv ;     end
+function tmux   ; command tmux -2 $argv ;               end
 function ip     ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
-function ping1  ; command ping -c 4 google.com ; end
+function ping1  ; command ping -c 4 google.com ;        end
 
-## Aliases. Programming Environment
+# Work Environment
 function dev;       cd $DEVELOPER;          end
 function public;    cd $PUBLIC;             end
 function private;   cd $DEVELOPER/private;  end
 function remote;    cd $DEVELOPER/remote;   end
 
-function b;         cd $PUBLIC/blog;        end
-function d;         cd $PUBLIC/dotfiles;    end
-function work;      cd $REMOTE/fluxon;      end
-
+# Most Active Projects
+function work;      cd $REMOTE/work;                        end
+function d;         cd $PUBLIC/dotfiles;                    end
+function b;         cd $PUBLIC/www.hamsternik.com;          end
