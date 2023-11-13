14a9bb7c3fe2b540e0caf65ff7459138766c251c
Nikita Khomitsevych <hamsternik9@gmail.com>
2023-11-13 20:08:27 +0000

    conf: update finicky.js

--- configs/finicky/finicky.js
+++ configs/finicky/finicky.js
@@ -9,40 +9,17 @@ module.exports = {
   handlers: [
     {
       match: [
-        "127.0.0.1"
+        "127.0.0.1",
+        "127.0.0.1:*"
       ],
-      browser: "Google Chrome"
-    },
-    {
-      match: [ // Open google.com and *.google.com urls in Google Chrome
-        "google.com*", // match google.com urls
-        finicky.matchDomains(/.*\.google.com/) // use helper function to match on domain only
-      ],
-      browser: "Google Chrome"
+      browser: "/Applications/Arc.app"
     },
     {
       match: [
-        "www.notion.so/superlayer*" // match only Fluxon::Gambit project in Notion
+        "app.slack.com",
+        "app.asana.com"
       ],
-      browser: "Google Chrome"
-    },
-    {
-      match: [
-        "https://dory.app/c/fluxon.com*"
-      ],
-      browser: "Google Chrome"
-    },
-    {
-        match: [
-            "https://fluxon.latticehq.com/*"
-        ],
-        browser: "Google Chrome"
-    },
-    {
-        match: [
-            "https://github.com/superlayer*"
-        ],
-        browser: "Google Chrome"
+      browser: "/Applications/Arc.app"
     }
   ]
 }
