115cfaeeffa3c85f51318dcb9d65f64bcbd1ed12
Nikita Khomitsevych <hamsternik9@gmail.com>
2025-01-23 16:16:11 +0000

    feat(nvim): WIP manage nvim config only in init.lua (moduleless)

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,3 +1,35 @@
-require("core.keymaps")
-require("core.plugins")
-require("plugin_config")
+-- TODO: check out mitchell' nvim dotfiles
+-- https://github.com/mitchellh/dotfiles/blob/main/nvim/init.vim
+
+-- require("core.keymaps")
+-- require("core.plugins")
+-- require("plugin_config")
+
+vim.g.mapleader = " "
+vim.g.maplocallleader = " "
+
+-- local map = vim.keymap.set
+
+vim.opt.showcmd = true
+vim.opt.backspace = '2'
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.api.nvim_set_keymap('n', '<Leader>s', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>e', ':Rex<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Ensure Neovim is Clipboard-Enabled
+-- One-Time Use: "+y or "*y
+-- Make It Automatic: set clipboard=unnamedplus
+-- Using pbcopy Directly (Alternative)
+vim.keymap.set('x', '<leader>y', '"+y')
